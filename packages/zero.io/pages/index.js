3c992799b81ad5741931b913a311655cb18ad427
marco <marco.bahn@gmail.com>
2018-10-29 16:38:25 +0100

    chore(zero): add demo component from lib

--- packages/zero.io/pages/index.js
+++ packages/zero.io/pages/index.js
@@ -1,1 +1,10 @@
-export default () => <p style={{ color: "red" }}>hi there</p>;
+import { Button, Link } from "components/lib/atoms";
+
+export default props => (
+	<div>
+		<h1>example</h1>
+		<p style={{ color: "red" }}>hi there</p>
+		<Button>Button</Button>
+		<Link href="#">a link</Link>
+	</div>
+);
