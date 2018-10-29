664e9ffeac40c5fc3db1c6010af4866377ecaef1
marco <marco.bahn@gmail.com>
2018-10-29 16:37:57 +0100

    chore(app): update ts deps

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
 
+import Button from "components/lib/Button";
+
 export default () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
+	<Layout title="Home | Next.js + TypeScript Example">
+		<h1>Hello Next.js 👋</h1>
+		<p>
+			<Link href="/about">
+				<a>About</a>
+			</Link>
+			<Button>BUTTON</Button>
+		</p>
+	</Layout>
 );
